0ea5242a6fc21fd1750122b02f17141a290ced97
Velizar <velizar_s@mail.bg>
2016-03-01 20:01:34 +0200

    Teamwork at Telerik Academy Software Quality Assurance Track

---
+++ TestingFramework.Utilities/Atributes/TestCaseIdAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestingFramework.Utilities.Atributes
+{
+	[System.AttributeUsage(System.AttributeTargets.Method)]
+	public class TestCaseId : Attribute
+	{
+		private string Id;
+
+		public TestCaseId(string id)
+		{
+			this.Id = id;
+		}
+
+		public TestCaseId(int id)
+		{
+			this.Id = id.ToString();
+		}
+	}
+}
